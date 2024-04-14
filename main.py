07f1d24e43daf9f278c1217d5bd1cf1ef88d4cb4
Maksim Bodnar <maximsevsk@gmail.com>
2024-04-14 22:04:10 +0300

    add gitignore

--- main.py
+++ main.py
@@ -5,24 +5,45 @@ class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.products = []
-
+        self.__products = []  # Приватный атрибут
         Category.total_categories += 1
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.total_unique_products += 1
+
+    def remove_product(self, product):
+        self.__products.remove(product)
+        Category.total_unique_products -= 1
+
+    def get_products(self):
+        return self.__products
+
+    def get_products_info(self):
+        products_info = []
+        for product in self.__products:
+            product_info = f"{product.name}, {product.price} руб. Остаток: {product.amount} шт."
+            products_info.append(product_info)
+        return products_info
 
 
 class Product:
-    name: str
-    description: str
-    products: list
-    price: int
-    amount: int
-
     def __init__(self, name, description, price, amount):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.amount = amount
 
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Ошибка: Цена должна быть больше нуля.")
+        else:
+            self.__price = new_price
 
 
 
