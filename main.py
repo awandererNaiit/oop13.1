217b66932588672d5dadea680ec68ef41659b8ce
Maksim Bodnar <maximsevsk@gmail.com>
2024-04-21 22:43:53 +0300

    error checking

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ class Category:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.__products = []  # Приватный атрибут
+        self._products = []  # Изменим имя атрибута с __products на _products
         Category.total_categories += 1
 
+    @property
+    def products(self):
+        return self._products
+
+    @product.setter
+    def products(self, products):
+        self._products = products
+
     def add_product(self, product):
-        self.__products.append(product)
+        self._products.append(product)
         Category.total_unique_products += 1
 
     def remove_product(self, product):
-        self.__products.remove(product)
+        self._products.remove(product)
         Category.total_unique_products -= 1
-
-    def get_products(self):
-        return self.__products
 
     def get_products_info(self):
         products_info = []
-        for product in self.__products:
+        for product in self._products:
             product_info = f"{product.name}, {product.price} руб. Остаток: {product.amount} шт."
             products_info.append(product_info)
         return products_info
@@ -45,5 +50,6 @@ class Product:
         else:
             self.__price = new_price
 
-
-
+    @staticmethod
+    def create_product(name, description, price, amount):
+        return Product(name, description, price, amount)
